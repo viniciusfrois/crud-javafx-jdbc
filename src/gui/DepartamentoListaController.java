bd13ec81df7926e8d1bd1ef5bd69a0097339b765
Vinicius Frois <viniifrois@gmail.com>
2021-04-30 11:01:28 -0300

    DepartamentoForm cadastro

--- src/gui/DepartamentoListaController.java
+++ src/gui/DepartamentoListaController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alertas;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Departamento;
 import model.services.DepartamentoServico;
@@ -36,8 +45,9 @@ public class DepartamentoListaController implements Initializable {
 	private ObservableList<Departamento> obsList;
 	
 	@FXML
-	public void onbtNovoAction () {
-		System.out.println("Botao");
+	public void onbtNovoAction (ActionEvent evento) {
+		Stage parentStage = Utils.stageAtual(evento);
+		criarDialogForm("/gui/DepartamentoForm.fxml", parentStage);
 	}
 	
 	public void setDepartamentoServico (DepartamentoServico servico) {
@@ -64,6 +74,25 @@ public class DepartamentoListaController implements Initializable {
 		List <Departamento> list = servico.buscarTodos();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartamento.setItems(obsList);
+	}
+	
+	private void criarDialogForm (String caminhoDaView, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(caminhoDaView));
+			Pane pane = loader.load();
+			
+			Stage dialogoStage = new Stage();
+			dialogoStage.setTitle("Cadastrar novo Departamento");
+			dialogoStage.setScene(new Scene(pane));
+			dialogoStage.setResizable(false);
+			dialogoStage.initOwner(parentStage);
+			dialogoStage.initModality(Modality.WINDOW_MODAL);
+			dialogoStage.showAndWait();
+		}
+		catch (IOException e) {
+			Alertas.showAlert("IOException", "Erro ao carregar a View", e.getMessage(), AlertType.ERROR);
+		}
+		
 	}
 
 }
